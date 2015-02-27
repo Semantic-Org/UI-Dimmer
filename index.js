7a6ebb73dfad8f0de245d0b201ce0193dfa94d27
Pusher Robot <jack@semantic-ui.com>
2015-02-27 18:14:21 -0500

    Updated component to version 1.10.3

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-/*
- * # Semantic - Dimmer
+/*!
+ * # Semantic UI 1.10.3 - Dimmer
  * http://github.com/semantic-org/semantic-ui/
  *
  *
- * Copyright 2014 Contributor
+ * Copyright 2014 Contributors
  * Released under the MIT license
  * http://opensource.org/licenses/MIT
  *
  */
 
 ;(function ( $, window, document, undefined ) {
+
+"use strict";
 
 module.exports = function(parameters) {
   var _module = module;
