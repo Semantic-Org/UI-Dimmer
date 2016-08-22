cbe60360ab54b9d3082ecfa2ac653090150f6b6d
Pusher Robot <jack@semantic-ui.com>
2016-08-21 23:33:22 -0400

    Updated component to version 2.2.3

---
+++ package.js
@@ -0,0 +1,14 @@
+
+Package.describe({
+  name    : 'semantic:ui-dimmer',
+  summary : 'Semantic UI - Dimmer: Single component release',
+  version : '2.2.3',
+  git     : 'git://github.com/Semantic-Org/UI-Dimmer.git',
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+  api.addFiles([
+    
+  ], 'client');
+});
