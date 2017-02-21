3b0bdb198e4dafc680a187734b9b6c9c9832ba7d
Jack Lukic <jack@semantic-ui.com>
2017-02-21 01:12:40 -0500

    Updated component to version 2.2.8

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-dimmer',
   summary : 'Semantic UI - Dimmer: Single component release',
-  version : '2.2.7',
+  version : '2.2.8',
   git     : 'git://github.com/Semantic-Org/UI-Dimmer.git',
 });
 
